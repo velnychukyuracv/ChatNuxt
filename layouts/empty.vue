1565a3dae7d9abf4c4253d0180780824be93cb49
Yura Velnychuk <yura.velnychuk@inventorsoft.co>
2019-12-26 18:51:09 +0200

    add empty file

---
+++ layouts/empty.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app app dark>
+    <v-content column justify-center align-center>
+      <v-container fluid>
+        <nuxt/>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,66 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <v-btn @click="message">NEW MESSAGE</v-btn>
+    <v-flex xs12 sm8>
+      <v-card minWidth="400">
+        <v-card-title class="text-center">
+          <h2>Nuxt chat</h2>
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="form" lazy-validation v-model="valid">
+            <v-text-field
+              label="Name"
+              required
+              :rules="nameRules"
+              v-model="name">
+            </v-text-field>
+
+            <v-text-field
+              label="Number chat"
+              required
+              :rules="chatRules"
+              v-model="chat">
+            </v-text-field>
+
+            <v-btn
+              class="mr-4"
+              color="primary"
+              :disabled="!valid"
+              @click="submit">
+              Submit
+            </v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
   export default {
+    layout: 'empty',
     sockets: {
-      connect: function() {
+      connect: function () {
         console.log('socket connected');
       }
     },
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+      ],
+      chat: '',
+      chatRules: [
+        v => !!v || 'Number chat is required'
+      ]
+    }),
     methods: {
-      message() {
-        this.$socket.emit('createMessage', {
-          text: 'FROM CLIENT'
-        });
+      submit() {
+        if (this.$refs.form.validate()) {
+
+        }
       }
     }
-  };
+  }
 </script>
